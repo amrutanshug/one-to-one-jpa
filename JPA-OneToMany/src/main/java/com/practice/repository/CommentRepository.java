a0a202eee1746b6a8acf88097ecbde0e0796b524
surya narayan <surya.narayan@iserveu.in>
2020-10-31 19:51:23 +0530

    comments added

--- JPA-OneToMany/src/main/java/com/practice/repository/CommentRepository.java
+++ JPA-OneToMany/src/main/java/com/practice/repository/CommentRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.practice.entity.Comment;
-
+//Comment added
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 	
